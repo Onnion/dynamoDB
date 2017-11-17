fcdd1680bdc254f07d7f2f4ebfef295a3829328c
Onnion <marcio.hsdl@gmail.com>
2017-11-17 15:38:56 -0300

    Update SearchSummary queries

--- tables/searchsummaries/dynamoose/createSearchSummaries.js
+++ tables/searchsummaries/dynamoose/createSearchSummaries.js
@@ -13,32 +13,54 @@ dynamoose.AWS.config.update({
 var searchSummariesSchema =  new Schema({
   id : {
     type: String,
-    hashKey: true
+    hashKey: true,
+    index: [{
+      global: true,
+      rangeKey: 'count',
+      name: 'CountRangeIndex',
+      project: true, // ProjectionType: ALL
+    },{
+      global: true,
+      rangeKey: 'company',
+      name: 'CompanyRangeIndex',
+      project: true, // ProjectionType: ALL
+    }]
   },
   updated_at:  {
     type: String,
   },
   created_at :  {
     type: String,
+    rangeKey: true,
+    index: true
   },
   company :  {
     type: String,
+    index: {
+      global: true,
+      rangeKey: 'created_at',
+      name: 'CompanyIndex',
+      project: true,
+      throughput: 5
+    }
   },
   count_from_cache :  {
     type: Number,
   },
   count :  {
     type: Number,
+    rangeKey: true,
+    index: true 
   }
 });
 
 // Create Companie Model
-var SearchSummaries = dynamoose.model('SearchSummaries', searchSummariesSchema);
+var SearchSummariesSS = dynamoose.model('SearchSummaries', searchSummariesSchema,{update: true });
 
 // Populate Table Companie
 itensToPopulate.forEach(function(summary) {
   // Create a new Companie object
-  var searchSummary = new SearchSummaries({
+  var searchSummary = new SearchSummariesSS({
     id : summary.id,
     updated_at :  summary.updated_at,
     created_at :  summary.created_at,
@@ -61,12 +83,18 @@ function searchSummaryGet(idvar){
 }
 
 // Query
+
 function searchSummaryQuery(){
-  Companie.get(idvar).then(function(companie){
-    console.log(companie);
-  }).catch(function(err){
-    console.log(err);
+  SearchSummariesSS.query('company').eq('azul')
+  .filter('count')
+  .between(200,2000)
+  .descending()
+  .exec().then((dogs) => {
+    console.log(dogs);
   })
+  .catch((err) => {
+    console.log(err);
+  });
 }
 
 // Scan
@@ -79,7 +107,7 @@ function searchSummaryScan(){
     }
   }
 
-  SearchSummaries.scan().not().exec()
+  SearchSummaries.scan().exec()
   .then(function(companie) {
     console.log(companie);
   })
@@ -88,5 +116,5 @@ function searchSummaryScan(){
   });
 };
 
-searchSummaryScan();
+searchSummaryQuery();
 //companieGet('58d02b9b7a343d148a8f7a30');
